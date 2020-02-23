c4a256ceb5d7448ddf336f499815bdbcd41af9df
Codeaday <AJORIGAMI1@gmail.com>
2020-02-23 16:53:05 -0600

    end of node path

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hellO");
+function Person() {}
+
+Person.prototype.calcAge = function() {
+    return 2020 - this.birthYear;
+};
+
+let john = new Person();
+
+console.dir(Object.getPrototypeOf(Person));
+console.dir("PERSON prototype:", JSON.stringify(Person.prototype));
+console.dir(
+    "JOHN'S [[PROTOTYPE]]:",
+    JSON.stringify(Object.getPrototypeOf(john))
+);
